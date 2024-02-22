cdf49d51143a289fbfccf6c1b4f2d3c23dc8d5c0
Klaes Sørensen <klsoe20@student.sdu.dk>
2024-02-22 08:41:04 +0100

    Update module-info.java

--- Enemy/src/main/java/module-info.java
+++ Enemy/src/main/java/module-info.java
@@ -9,5 +9,4 @@ module Enemy {
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
-
 }
